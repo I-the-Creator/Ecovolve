909d97b66693eb43114ad9a1e37a4cc3c65a6f8d
Lachlan-Ikeguchi <lachlan.ikeguchi@outlook.com>
2023-10-07 13:00:39 +1000

    sine function

--- Ecovolve/src/Ecovolve.jl
+++ Ecovolve/src/Ecovolve.jl
@@ -1,7 +1,56 @@
 module Ecovolve
-using Gtk, Plots
+using Gtk
+using GtkReactive
+using Plots
+inspectdr()
 
-greet() = print("Hello World!")
+function main()
+    window = GtkWindow("Julia GTK App", 800, 600)
+
+    verticalBox = GtkBox(:v)
+    add(window, verticalBox)
+
+    sliderAmplitude = GtkScale(0.1, 5.0, 0.1)
+    sliderFrequency = GtkScale(1.0, 10.0, 0.1)
+    sliderPhase = GtkScale(0.0, 2π, 0.1)
+
+    Graph = GtkDrawingArea()
+
+    plot = plot([], [], xlabel="X", ylabel="Y", legend=false)
+
+    function update_graph()
+        amplitude = sliderAmplitude.value
+        frequency = sliderFrequency.value
+        phase = sliderPhase.value
+
+        x = 0:0.1:10  # Define x values
+        y = amplitude * sin.(frequency * x .+ phase)  # Calculate y values
+
+        # Update the plot data
+        plot[1] = x
+        plot[2] = y
+
+        # Redraw the plot
+        display(Graph)
+    end
+
+    # Connect the sliders to the update function
+    signal_connect(sliderAmplitude, :value, update_graph)
+    signal_connect(sliderFrequency, :value, update_graph)
+    signal_connect(sliderPhase, :value, update_graph)
+
+    # Add widgets to the main window layout
+    GtkBox.add(verticalBox, sliderAmplitude)
+    GtkBox.add(verticalBox, sliderFrequency)
+    GtkBox.add(verticalBox, sliderPhase)
+    GtkBox.add(verticalBox, Graph)
+
+    # Show the window
+    showall(window)
+
+    # Start the GTK main loop
+    Gtk.main()
+end
 
 function julia_main()::Cint
     try
@@ -12,9 +61,4 @@ function julia_main()::Cint
     end
     return 0
 end
-
-function main()
-
-end
-
 end # module Ecovolve
---
+++ run.sh
@@ -0,0 +1,1 @@
+julia Ecovolve/src/Ecovolve.jl
